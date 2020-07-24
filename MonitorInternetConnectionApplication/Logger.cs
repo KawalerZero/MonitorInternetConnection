6139e8a2a905f68b5ac044afc186e3e66a16a786
Adam Kowalczyk <adamkowalczyk12@outlook.com>
2020-07-24 23:18:44 +0200

    Extract Logger. Refactor in PingHandler class.

---
+++ MonitorInternetConnectionApplication/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MonitorInternetConnectionApplication
+{
+	public static class Logger
+	{
+		private static readonly string PATH = @"d:\LogsMonitorInternetConnection\";
+		private static readonly string SECOND_PART_OF_FILENAME = "Logs.txt";
+
+		public static void Log(string message)
+		{
+			try
+			{
+				string fullPath = PATH + DateTime.Today.ToShortDateString() + SECOND_PART_OF_FILENAME;
+				var file = new StreamWriter(fullPath, true);
+				file.WriteLine(message);
+				file.Close();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message.ToString());
+			}
+		}
+	}
+}
--- MonitorInternetConnectionApplication/PingHandler.cs
+++ MonitorInternetConnectionApplication/PingHandler.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.IO;
 using System.Net.NetworkInformation;
-using System.Windows.Forms;
 namespace MonitorInternetConnectionApplication
 {
 	public static class PingHandler
 	{
-		private static string PATH = @"d:\LogsMonitorInternetConnection\";
-		private static string SECOND_PART_OF_FILENAME = "Logs.txt";
-		private static string[] ARRAY_OF_IP_ADDRESSES = { "139.130.4.5", "204.15.21.255", "212.77.101.5", "178.33.51.179", "8.8.8.8" };
+		private static readonly string[] ARRAY_OF_IP_ADDRESSES = { "139.130.4.5", "204.15.21.255", "212.77.101.5", "178.33.51.179", "8.8.8.8" };
 		private static int PING_NUMBER = 0;
 
 		public static void Ping()
@@ -17,34 +13,23 @@ namespace MonitorInternetConnectionApplication
 			try
 			{
 				var pingReply = ping.Send(ARRAY_OF_IP_ADDRESSES[PING_NUMBER]);
+				Logger.Log($"{DateTime.Now}: Ping to: {pingReply.Address} Status: {pingReply.Status}");
+			}
+			catch (PingException pingEx)
+			{
+				Logger.Log($"{DateTime.Now}: Ping to: {ARRAY_OF_IP_ADDRESSES[PING_NUMBER]} Ping exception: {pingEx.Message}");
+			}
+			catch (Exception ex)
+			{
+				Logger.Log($"{DateTime.Now}: Ping to: {ARRAY_OF_IP_ADDRESSES[PING_NUMBER]} Unexpected exception: {ex.Message}");
+			}
+			finally
+			{
 				PING_NUMBER++;
 				if (PING_NUMBER > ARRAY_OF_IP_ADDRESSES.Length - 1)
 				{
 					PING_NUMBER = 0;
 				}
-				Log(DateTime.Now + ": Ping to: " + pingReply.Address + " Status: " + pingReply.Status);
-			}
-			catch (PingException pingEx)
-			{
-				Log(DateTime.Now + ": Ping exception: " + pingEx.Message);
-			}
-			catch (Exception ex)
-			{
-				Log(DateTime.Now + ": Unexpected exception: " + ex.Message);
-			}
-		}
-		private static void Log(string message)
-		{
-			try
-			{
-				string fullPath = PATH + DateTime.Today.ToShortDateString() + SECOND_PART_OF_FILENAME;
-				var file = new StreamWriter(fullPath, true);
-				file.WriteLine(message);
-				file.Close();
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.Message.ToString());
 			}
 		}
 	}
